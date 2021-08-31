273091db30d56775bcae5bbcd126cd545ed303d3
clemilton <clemilton.fucapi@gmail.com>
2021-08-31 19:11:18 -0400

    aula 31-08

--- projeto-pedidos/cadastrarPizza.js
+++ projeto-pedidos/cadastrarPizza.js
@@ -7,9 +7,9 @@ function cadastrarPizza(){
     for(let i = 0 ; i< LISTA_PIZZAS.length ; i++){
         string+= LISTA_PIZZAS[i].nome +"\n";
         string+= "    - "+LISTA_PIZZAS[i].ingredientes + "\n";
-        string+="    - Preco P: "+LISTA_PIZZAS[i].precoP +"\n";
-        string+="    - Preco M: "+LISTA_PIZZAS[i].precoM +"\n";
-        string+="    - Preco G: "+LISTA_PIZZAS[i].precoG +"\n";
+        string+="    - Preco P: "+LISTA_PIZZAS[i].precos['P'] +"\n";
+        string+="    - Preco M: "+LISTA_PIZZAS[i].precos['M'] +"\n";
+        string+="    - Preco G: "+LISTA_PIZZAS[i].precos['G'] +"\n";
     }
     alert(string)
     
--- projeto-pedidos/dados.js
+++ projeto-pedidos/dados.js
@@ -2,26 +2,37 @@
 
 
 var LISTA_PIZZAS = [
-    /* cada elemento é um objeto -> pizza JSON */
+    /* cada elemento é um objeto -> pizza  */
     {
         nome: "Pizza Calabresa",
         ingredientes: "calabreasa,muçarela, cebola, molho de tomate",
-        precoP: 25,
-        precoM: 35,
-        precoG: 45 
+        precos: {
+            "P":25,
+            "M":35,
+            "G":45
+            /* organizando os preços dessa maneira, o acesso a 
+            eles fica mais simples
+            ex: LISTA_PIZZAS[0].precos['P']
+                - Preço da pizza pequena do primeiro elemento
+            */
+        }
     },
     {
         nome: "Pizza Portuguesa",
         ingredientes:"tomate, ovo, ervilha, queijo, presunto",
-        precoP: 25,
-        precoM: 35,
-        precoG: 45
+        precos: {
+            "P":25,
+            "M":35,
+            "G":45
+        }
     },
     {
         nome: "Pizza Frango Catupiri",
         ingredientes: "frango desfiado com catupiry",
-        precoP: 30,
-        precoM: 40,
-        precoG: 45
+        precos: {
+            "P":30,
+            "M":40,
+            "G":50
+        }
     }
 ]
