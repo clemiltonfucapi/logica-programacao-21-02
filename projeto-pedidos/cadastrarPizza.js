1ddff435b00ef8a3303f75f16f2871cbc39c172b
clemilton <clemilton.fucapi@gmail.com>
2021-08-30 21:42:46 -0400

    aula 30-08

--- projeto-pedidos/cadastrarPizza.js
+++ projeto-pedidos/cadastrarPizza.js
@@ -1,10 +1,9 @@
 
 
 function cadastrarPizza(){
-    alert("cadastrar pizza")
     //listar as pizzas
-    let string=""
-    /* */
+    let string="LISTA DE PIZZAS: \n"
+    
     for(let i = 0 ; i< LISTA_PIZZAS.length ; i++){
         string+= LISTA_PIZZAS[i].nome +"\n";
         string+= "    - "+LISTA_PIZZAS[i].ingredientes + "\n";
@@ -12,5 +11,35 @@ function cadastrarPizza(){
         string+="    - Preco M: "+LISTA_PIZZAS[i].precoM +"\n";
         string+="    - Preco G: "+LISTA_PIZZAS[i].precoG +"\n";
     }
-    prompt(string)
+    alert(string)
+    
+    /*criar uma pizza(objeto) vazia */
+    let pizza = {}
+    do{
+        pizza.nome = prompt("Digite o nome da pizza")
+    }while(pizza.nome=="");
+
+    do{
+        pizza.ingredientes = prompt("Digite os ingredientes da pizza")
+    }while(pizza.ingredientes=="")
+
+    do{
+        pizza.precoP = parseFloat(
+            prompt("Digite o preço da pizza pequena")
+        )
+    }while(  isNaN(pizza.precoP) )
+
+    do{
+        pizza.precoM = parseFloat(
+            prompt("Digite o preço da pizza media")
+        )
+    }while(  isNaN(pizza.precoM) )
+
+    do{
+        pizza.precoG = parseFloat(
+            prompt("Digite o preço da pizza grande")
+        )
+    }while(  isNaN(pizza.precoG) )
+    //INSERIR na LISTA_PIZZAS
+    LISTA_PIZZAS.push(pizza)
 }
